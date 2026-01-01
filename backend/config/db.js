4f6316c973af4bf104a9a2bda60c4764528343d2
InderpalSingh01 <inderdhillon3114@gmail.com>
2026-01-01 13:42:44 +0530

    Update database connection string in db.js

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,9 +1,6 @@
 import mongoose from "mongoose";
 
 export const  connectDB = async () =>{
-    await mongoose.connect('mongodb+srv://inderdhillon3114_db_user:Zv1BYpqxrD0l4WtC@cluster0.brqh222.mongodb.net/?appName=Cluster0').then(()=>console.log("Database is Connected"))
+    await mongoose.connect('').then(()=>console.log("Database is Connected"))
 }
 
-
-// mongodb+srv://inderdhillon3114_db_user:Zv1BYpqxrD0l4WtC@cluster0.brqh222.mongodb.net/?appName=Cluster0
-// mongodb+srv://inderdhillon3114_db_user:inder123@cluster0.mwrtm8p.mongodb.net/?appName=Cluster0
