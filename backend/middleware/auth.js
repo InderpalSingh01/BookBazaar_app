5a59c3ba59bb3da3188c81c0155c095ed0f2f124
InderpalSingh01 <inderdhillon3114@gmail.com>
2026-01-01 12:58:50 +0530

    Update auth.js

--- backend/middleware/auth.js
+++ backend/middleware/auth.js
@@ -5,6 +5,7 @@ const authMiddleware = async (req, res, next) => {
     if (!token) {
         return res.json({success:false,message:'Not Authorized Login Again'});
     }
+    
     try {
         const token_decode =  jwt.verify(token, process.env.JWT_SECRET);
         req.body.userId = token_decode.id;
