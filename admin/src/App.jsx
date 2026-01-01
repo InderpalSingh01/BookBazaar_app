af563085fbe9d7146cd1e0cdd4698e0cfa5bd3bd
InderpalSingh01 <inderdhillon3114@gmail.com>
2026-01-01 12:42:06 +0530

    Update App.jsx

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -7,7 +7,7 @@ import List from './pages/List/List'
 import Orders from './pages/Orders/Orders'
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
-
+// admin panel
 const App = () => {
   return (
     <div className='app'>
